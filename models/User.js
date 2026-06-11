c31082418d382623db35110a68856dd11d9220f7
idatasubcom-oss <idatasub.com@gmail.com>
2026-06-11 12:54:08 +0100

    Add User model with schema definition

--- models/User.js
+++ models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  password: String,
+  balance: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("User", userSchema);
