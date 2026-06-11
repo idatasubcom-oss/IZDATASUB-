66975281db452f9d252be631521832c17c066d5d
idatasubcom-oss <idatasub.com@gmail.com>
2026-06-11 14:11:21 +0100

    Refactor user schema to include fullName and email

--- models/User.js
+++ models/User.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  phone: String,
-  password: String,
-  balance: { type: Number, default: 0 }
-});
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
