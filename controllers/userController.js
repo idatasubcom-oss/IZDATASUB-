b70e2e3eac98ed5dd7bde4dfdeb77a815356cad1
idatasubcom-oss <idatasub.com@gmail.com>
2026-06-14 01:41:19 +0100

    Refactor userController to use Express router
    Refactor user registration logic into router and add test route.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,17 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const express = require("express");
+const router = express.Router();
+
+const { registerUser } = require("../controllers/userController");
 
 // Register User
-const registerUser = async (req, res) => {
-  try {
-    const { name, phone, password } = req.body;
+router.post("/register", registerUser);
 
-    if (!name || !phone || !password) {
-      return res.status(400).json({
-        message: "Please provide all fields"
-      });
-    }
+// Test Route
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Auth API Working"
+  });
+});
 
-    const userExists = await User.findOne({ phone });
-
-    if (userExists) {
-      return res.status(400).json({
-        message: "User already exists"
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      phone,
-      password: hashedPassword
-    });
-
-    res.status(201).json({
-      message: "User registered successfully",
-      user: {
-        id: user._id,
-        name: user.name,
-        phone: user.phone,
-        balance: user.balance
-      }
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      message: error.message
-    });
-  }
-};
-
-module.exports = { registerUser };
+module.exports = router;
